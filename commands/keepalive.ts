995cdacc64be3566f3ae89438f64aa79885a6eb6
q13x <84165981+WorldEditAxe@users.noreply.github.com>
2022-01-07 21:37:26 -0800

    Revert "POLLY SAYS HI"
    This reverts commit 69a9f727d9b9a2955edfa597777bf4c810b3b846.

--- commands/keepalive.ts
+++
@@ -1,7 +0,0 @@
-import * as express from 'express'
-
-const server = express.default()
-const port = 3000
-
-server.get('/', (req, res) => res.send('{"status":"online","based":"true"}'))
-server.listen(port)
