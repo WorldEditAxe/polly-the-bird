40f283c84c8ad56ffa1e7c8d7d30c2277902dc06
Your Name <you@example.com>
2022-01-14 21:10:50 -0800

    Add colored logging (also new dependency go brr)

--- lib/logger.ts
+++ lib/logger.ts
@@ -1,27 +1,34 @@
+import { Chalk } from 'chalk'
+
+const color = new Chalk({ level: 2 })
+
 export class Logger {
     loggerName: string
+    verbose: boolean
 
-    constructor(name: string ) {
+    constructor(name: string, verbose?: boolean) {
         this.loggerName = name
+        if (verbose) this.verbose = verbose
     }
 
     info(s: string) {
-        console.log(`[${new Date().toUTCString()}] [INFO/${this.loggerName}]: ${s}`)
+        console.log(`${color.gray('[')}${color.green(new Date().toISOString())}${color.gray('] [')}${color.reset(this.loggerName)}${color.gray('/')}${color.bgBlue('INFO')}${color.gray('] - ')}${color.reset(s)}`)
     } 
 
     warn(s: string) {
-        console.warn(`[${new Date().toUTCString()}] [WARN/${this.loggerName}]: ${s}`)
+        console.log(`${color.gray('[')}${color.green(new Date().toISOString())}${color.gray('] [')}${color.reset(this.loggerName)}${color.gray('/')}${color.bgYellowBright('WARN')}${color.gray('] - ')}${color.reset(s)}`)
     } 
 
     error(s: string) {
-        console.error(`[${new Date().toUTCString()}] [ERROR/${this.loggerName}]: ${s}`)
+        console.log(`${color.gray('[')}${color.green(new Date().toISOString())}${color.gray('] [')}${color.reset(this.loggerName)}${color.gray('/')}${color.bgRedBright('ERROR')}${color.gray('] - ')}${color.reset(s)}`)
     } 
 
     fatal(s: string) {
-        console.error(`[${new Date().toUTCString()}] [FATAL/${this.loggerName}]: ${s}`)
+        console.log(`${color.gray('[')}${color.green(new Date().toISOString())}${color.gray('] [')}${color.reset(this.loggerName)}${color.gray('/')}${color.bgRed('FATAL')}${color.gray('] - ')}${color.reset(s)}`)
     } 
 
     debug(s: string) {
-        console.debug(`[${new Date().toUTCString()}] [DEBUG/${this.loggerName}]: ${s}`)
+        if (!this.verbose) return
+        console.log(`${color.gray('[')}${color.green(new Date().toISOString())}${color.gray('] [')}${color.reset(this.loggerName)}${color.gray('/')}${color.bgGray('DEBUG')}${color.gray('] - ')}${color.reset(s)}`)
     } 
 }
