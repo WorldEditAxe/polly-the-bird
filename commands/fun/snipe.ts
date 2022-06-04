4e65f9fa8d1c27cd4c85e6faa3ef8425d23b95b1
q13x <84165981+WorldEditAxe@users.noreply.github.com>
2022-06-04 09:12:16 +0000

    [WIP] /snipe command revamp

--- commands/fun/snipe.ts
+++ commands/fun/snipe.ts
@@ -3,8 +3,25 @@ import { Client, CommandInteraction, GuildMemberRoleManager, Message, MessageEmb
 import { CommandPreprocessor } from "../../lib/preprocessor/commandPreprocessor.js"
 import { CooldownDate } from "../../lib/preprocessor/cooldownDate.js"
 
-const snipe = new Map<string, Message>()
+const snipe = new Map<string, Array<DeletedMessage | EditedMessage>>()
 const snooze = ms => new Promise<void>(res => setTimeout(res, ms))
+
+type loggedType = 'DELETED' | 'EDITED'
+
+type DeletedMessage = {
+    messageId: string,
+    content: string,
+    attachments: string[]
+    type: 'DELETED'
+}
+
+type EditedMessage = {
+    messageId: string
+    messageJumpLink: string,
+    oldContent: string
+    newContent: string,
+    type: 'EDITED'
+}
 
 export const preprocessor = new CommandPreprocessor({
     cooldown: new CooldownDate({ seconds: 10 }),
@@ -15,35 +32,20 @@ export const preprocessor = new CommandPreprocessor({
 export const slashCommand = new SlashCommandBuilder()
     .setName('snipe')
     .setDescription('ur mum')
+    .addBooleanOption(o => o.setName("list").setDescription("Whether or not to show a snipelist or not.").setRequired(false))
 
 export async function execute(i: CommandInteraction) {
     if (!i.guild) return await i.reply({ content: 'imagine not using this in a server smfh', ephemeral: true })
     if (!i.guild.me.permissionsIn(i.channel as any).has(Permissions.FLAGS.VIEW_CHANNEL)) return await i.reply({ content: "I cannot snipe in a channel where I do not have the permissions to view sent messages in.", ephemeral: true })
     if (!(i.member.roles as GuildMemberRoleManager).cache.has('799022090791419954') && !(i.member.roles as GuildMemberRoleManager).cache.has('929898376001122325') && !(i.member.roles as GuildMemberRoleManager).cache.has('785676961904852992')) return await i.reply({ content: "You need to be a 25 million donor in order to use this command!", ephemeral: true })
-    const snipedMessage = snipe.get(i.channel.id)
-    if (!snipedMessage) return await i.reply({ content: 'hey bud there ain\'t any sniped messages here' })
-    const attachments = snipedMessage.attachments
+    const list = typeof i.options.getBoolean("list") == 'boolean' ? i.options.getBoolean("list") : false
 
-    const embed = new MessageEmbed()
-    .setAuthor(snipedMessage.author.tag, snipedMessage.author.avatarURL())
-    .setTitle(`Sniped message in #${(i.channel as TextChannel).name}`)
-    .setDescription(snipedMessage.content && snipedMessage.content.length > 0
-                    ? snipedMessage.content.length > 2000 ? snipedMessage.content.slice(0, 1997) + '...' : snipedMessage.content
-                    : "<nothing, maybe an embed/attachment?>")
-    .setTimestamp(snipedMessage.createdAt)
-
-    if (attachments.size > 0) {
-        let linkStr = '', index = 0
-
-        attachments.forEach((v, k) => {
-            index++
-            linkStr += `[${index}]: [${v.name}](${v.url})\n`
-        })
-
-        embed.addField("Attachments", linkStr.trimEnd(), true)
+    // TODO: work on
+    if (list) {
+        // send snipelist
+    } else {
+        // send latest sniped msg
     }
-
-    await i.reply({ embeds: [ embed ] })
 }
 
 export async function staticBlock() {
@@ -51,8 +53,33 @@ export async function staticBlock() {
     
     client.on('messageDelete', async m => {
         if (!m.guild || m.author.bot) return
-        snipe.set(m.channel.id, m as Message)
-        await snooze(10 * 60 * 1000)
-        if (snipe.has(m.channel.id)) snipe.delete(m.channel.id)
+        const attachments = []
+        m.attachments.forEach(a => attachments.push(a.url))
+
+        appendAndClamp(snipe.get(m.channelId), {
+            messageId: m.id,
+            content: m.content,
+            attachments: attachments,
+            type: 'DELETED'
+        } as DeletedMessage, 10)
     })
+
+    client.on('messageUpdate', (before, after) => {
+        if (!after.guild || after.author.bot) return
+        appendAndClamp(snipe.get(after.channelId), {
+            messageId: after.id,
+            messageJumpLink: after.url,
+            oldContent: before.content,
+            newContent: after.content
+        } as EditedMessage, 10)
+    })
+}
+
+function appendAndClamp(array: Array<any>, element: any, limit: number) {
+    if (array.length > limit) {
+        array.unshift(element)
+        array.pop
+    } else {
+        array.unshift(element)
+    }
 }
