bb069b7bf25c9f7f64fe0013aa807c1eb5c7859a
q13x <84165981+WorldEditAxe@users.noreply.github.com>
2022-06-13 10:00:55 +0000

    Add user ad automatic role giver

---
+++ commands/modUtils/ad.ts
@@ -0,0 +1,62 @@
+import { Client, Presence } from "discord.js";
+
+const client: Client = global.bot.djsClient
+
+const PREMIUM_ROLE = '822534119916437616' // placeholder, change to actual role when? this is the id of the me > you role
+const CONTENT = ["discord.gg/merchants", ".gg/merchants"]
+
+client.on('presenceUpdate', async (oldStatus, newStatus) => {
+    if (newStatus.user.bot || newStatus.guild.id != '784491141022220309') return
+    let hasContentInStatus = false
+    if ((oldStatus as Presence).member.roles.cache.has(PREMIUM_ROLE)) {
+        for (const status of newStatus.activities) {
+            if (status.type == 'CUSTOM') {
+                if (!status.state) {
+                    break
+                } else {
+                    const lowered = status.state.toLowerCase()
+
+                    for (const ent of CONTENT) {
+                        let l = false
+                        for (const part of lowered.split(/ /g)) {
+                            if (part == ent) {
+                                l = true
+                                hasContentInStatus = true
+                                break
+                            }
+                        }
+                        if (l) break
+                    }
+
+                    break
+                }
+            }
+        }
+    } else {
+        for (const status of newStatus.activities) {
+            if (status.type == 'CUSTOM') {
+                if (!status.state) break
+                else {
+                    const lowered = status.state.toLowerCase()
+
+                    for (const ent of CONTENT) {
+                        let l = false
+                        for (const part of lowered.split(/ /g)) {
+                            if (part == ent) {
+                                l = true
+                                hasContentInStatus = true
+                                break
+                            }
+                        }
+                        if (l) break
+                    }
+
+                    break
+                }
+            }
+        }
+    }
+
+    if (newStatus.member.roles.cache.has(PREMIUM_ROLE) && !hasContentInStatus) await newStatus.member.roles.remove(PREMIUM_ROLE).catch(() => {})
+    else if (hasContentInStatus) await newStatus.member.roles.add(PREMIUM_ROLE).catch(() => {})
+})
