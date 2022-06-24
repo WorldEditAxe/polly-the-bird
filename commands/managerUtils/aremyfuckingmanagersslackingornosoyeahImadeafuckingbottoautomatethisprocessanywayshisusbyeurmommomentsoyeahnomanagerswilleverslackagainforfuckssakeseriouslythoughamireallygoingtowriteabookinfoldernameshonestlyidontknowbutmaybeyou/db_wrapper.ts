317a3fd5017d564b27c81ae7edd980c47c96f6e2
q13x <84165981+WorldEditAxe@users.noreply.github.com>
2022-06-24 06:28:30 +0000

    retardspaces is too slow

--- commands/managerUtils/aremyfuckingmanagersslackingornosoyeahImadeafuckingbottoautomatethisprocessanywayshisusbyeurmommomentsoyeahnomanagerswilleverslackagainforfuckssakeseriouslythoughamireallygoingtowriteabookinfoldernameshonestlyidontknowbutmaybeyou/db_wrapper.ts
+++ commands/managerUtils/aremyfuckingmanagersslackingornosoyeahImadeafuckingbottoautomatethisprocessanywayshisusbyeurmommomentsoyeahnomanagerswilleverslackagainforfuckssakeseriouslythoughamireallygoingtowriteabookinfoldernameshonestlyidontknowbutmaybeyou/db_wrapper.ts
@@ -1,3 +1,4 @@
+import { User } from "discord.js";
 import { awaitStart, getDb } from "../../database.js";
 
 await awaitStart()
@@ -42,20 +43,48 @@ export function dayToString(date: number): string {
     }
 }
 
+export function dayToEnum(date: number): DayOfWeek {
+    if (date > 7 || date < 1) throw new Error("Date number is out of bounds!")
+
+    switch(date) {
+        case 1:
+            return DayOfWeek.SATURDAY
+            break
+        case 2:
+            return DayOfWeek.MONDAY
+            break
+        case 3:
+            return DayOfWeek.TUESDAY
+            break
+        case 4:
+            return DayOfWeek.WEDNESDAY
+            break
+        case 5:
+            return DayOfWeek.THURSDAY
+            break
+        case 6:
+            return DayOfWeek.FRIDAY
+            break
+        case 7:
+            return DayOfWeek.SATURDAY
+            break
+    }
+}
+
 export type schema$dailyReport = {
     day: number, // unix seconds
     day_of_week: number,
-    events_made: number
+    quota_filled: number
 }
 
 export type schema$weeklyReport = {
-    sun: schema$dailyReport,
-    mon: schema$dailyReport,
-    tue: schema$dailyReport,
-    wed: schema$dailyReport,
-    thu: schema$dailyReport,
-    fri: schema$dailyReport,
-    sat: schema$dailyReport
+    sun?: schema$dailyReport,
+    mon?: schema$dailyReport,
+    tue?: schema$dailyReport,
+    wed?: schema$dailyReport,
+    thu?: schema$dailyReport,
+    fri?: schema$dailyReport,
+    sat?: schema$dailyReport
 }
 
 export type schema$BaseManager = {
@@ -66,3 +95,102 @@ export type schema$BaseManager = {
 
 export const events = db.collection("event_managers")
 export const giveaways = db.collection("giveaway_managers")
+
+function getDaysBeforeToday(unix: number, days: number): number {
+    const SINGLE_DAY = 86400
+    return unix - (SINGLE_DAY * days)
+}
+
+function getUnixSeconds(): number {
+    return Math.floor(Date.now() / 1000)
+}
+
+function getCurrentDayOfWeek(): number {
+    return new Date().getDay()
+}
+
+export async function initManager(user: User, type: 'EVENTS' | 'GIVEAWAYS') {
+    if (type == 'EVENTS') {
+        let ins: schema$BaseManager = {
+            user_id: user.id,
+            current_weekly_report: {
+                sun: {
+                    day: getDaysBeforeToday(getUnixSeconds(), getCurrentDayOfWeek),
+                    day_of_week: 0,
+                    quota_filled: 0
+                },
+                mon: {
+                    day: 2,
+                    day_of_week: 0,
+                    quota_filled: 0
+                },
+                tue: {
+                    day: 0,
+                    day_of_week: 0,
+                    quota_filled: 0
+                },
+                wed: {
+                    day: 0,
+                    day_of_week: 0,
+                    quota_filled: 0
+                },
+                thu: {
+                    day: 0,
+                    day_of_week: 0,
+                    quota_filled: 0
+                },
+                fri: {
+                    day: 0,
+                    day_of_week: 0,
+                    quota_filled: 0
+                },
+                sat: {
+                    day: 0,
+                    day_of_week: 0,
+                    quota_filled: 0
+                }
+            },
+            last_weekly_report: {
+                sun: {
+                    day: 0,
+                    day_of_week: 0,
+                    quota_filled: 0
+                },
+                mon: {
+                    day: 0,
+                    day_of_week: 0,
+                    quota_filled: 0
+                },
+                tue: {
+                    day: 0,
+                    day_of_week: 0,
+                    quota_filled: 0
+                },
+                wed: {
+                    day: 0,
+                    day_of_week: 0,
+                    quota_filled: 0
+                },
+                thu: {
+                    day: 0,
+                    day_of_week: 0,
+                    quota_filled: 0
+                },
+                fri: {
+                    day: 0,
+                    day_of_week: 0,
+                    quota_filled: 0
+                },
+                sat: {
+                    day: 0,
+                    day_of_week: 0,
+                    quota_filled: 0
+                }
+            }
+        }
+
+        
+    } else if (type == 'GIVEAWAYS') {
+
+    }
+}
--- commands/managerUtils/aremyfuckingmanagersslackingornosoyeahImadeafuckingbottoautomatethisprocessanywayshisusbyeurmommomentsoyeahnomanagerswilleverslackagainforfuckssakeseriouslythoughamireallygoingtowriteabookinfoldernameshonestlyidontknowbutmaybeyou/logger.ts
+++ commands/managerUtils/aremyfuckingmanagersslackingornosoyeahImadeafuckingbottoautomatethisprocessanywayshisusbyeurmommomentsoyeahnomanagerswilleverslackagainforfuckssakeseriouslythoughamireallygoingtowriteabookinfoldernameshonestlyidontknowbutmaybeyou/logger.ts
@@ -7,7 +7,14 @@ const client: Client = global.bot.djsClient
 const GIVEAWAY_ROLE_ID = '785198646731604008'
 const EVENT_ROLE_ID = '791516116710064159'
 
+const EVENT_PING_CHANNEL = '870198964635467787'
+const EVENT_ROLE_PING = '925228998492061806'
+
 client.on('messageCreate', async msg => {
     if (!msg.guild || (!msg.member.roles.cache.has(GIVEAWAY_ROLE_ID) && !msg.member.roles.cache.has(EVENT_ROLE_ID))) return
-
+    if (msg.member.roles.cache.has(EVENT_ROLE_ID)) {
+        if (msg.guild && msg.channelId == EVENT_PING_CHANNEL && msg.mentions.roles.has(EVENT_ROLE_PING)) {
+            
+        }
+    }
 })
