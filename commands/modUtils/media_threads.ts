5c4464ccc824eeb9d4bdda9228eafd103398d05e
WorldEditAxe <yourmom@yourmom.com>
2022-07-04 15:48:07 -0700

    Add media threads (suggested by Rithikasaurus#0604)

---
+++ commands/modUtils/media_threads.ts
@@ -0,0 +1,28 @@
+import { Client, MessageEmbed } from "discord.js"
+
+const MEDIA_CHANNEL = '870235473862344704'
+const client: Client = global.bot.djsClient
+
+client.on('messageCreate', async msg => {
+    if (msg.channelId != MEDIA_CHANNEL) return
+    if (msg.attachments.size <= 0) {
+        await msg.delete().catch(() => {})
+        return
+    }
+
+    await msg.startThread({
+        name: "Comments",
+        autoArchiveDuration: 1440,
+        reason: "Automated media thread creation."
+    })
+        .then(thread => {
+            thread.send({
+                embeds: [
+                    new MessageEmbed()
+                        .setTitle("Media Comment Thread")
+                        .setColor('#4573ed')
+                        .setDescription("Comment on a media post sent by a server member.\nPlease read the server rules at <#787343840108478474> prior to commenting.")
+                ]
+            })
+        })
+})
